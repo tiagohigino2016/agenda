8c2cebd4c2aad421dbccfec1f7a2aa55f5b07c14
unknown <you@example.comtiagohigino2016@gmail.comgit config --global user.name Your>
2020-04-16 18:03:20 -0300

    Criada a tela de listagem de eventos

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from core.models import Evento
 # Register your models here.
+
+
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo','descricao', 'data_evento', 'data_criacao')
+    list_filter = ('usuario','data_evento',)
+
+admin.site.register(Evento, EventoAdmin)
+
+
+
